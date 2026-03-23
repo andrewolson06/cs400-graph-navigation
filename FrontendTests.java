cc54f29e8d7414897b1f8c888dc4c1b9e1cc2296
Genji MATSUYAMA <matsuyama@wisc.edu>
2026-03-23 12:52:50 -0500

    cs400 script submission from Mon Mar 23 12:52:50 PM CDT 2026

--- FrontendTests.java
+++ FrontendTests.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
-public class FrontendTester {
+public class FrontendTests {
 
 
 
